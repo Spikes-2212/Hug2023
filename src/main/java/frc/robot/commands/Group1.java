40e2b64e3e1c3e76182f31ea6420026d4a35e9b8
Spikes <Spikes.no2212@gmail.com>
2023-01-17 12:41:33 +0200

    grou[1

--- src/main/java/frc/robot/commands/Group1.java
+++ src/main/java/frc/robot/commands/Group1.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import com.spikes2212.command.drivetrains.commands.DriveTank;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drivetrain;
@@ -8,7 +9,10 @@ public class Group1 extends SequentialCommandGroup {
 
     public Group1(Drivetrain drivetrain) {
         addCommands(
-
+        new DriveTank(drivetrain, 0.7, 0.7).withTimeout(3),
+        new Turn(drivetrain, 0.7, 69),
+        new DriveTank(drivetrain, 0.7, 0.7).withTimeout(3),
+        new DriveTank(drivetrain, 0,0.5).withTimeout(5)
         );
     }
 }
